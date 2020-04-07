ebae2fc80ae13e6903abac007a19064c37b80684
ixoye <contacto@ixoye215.com>
2020-04-07 16:33:28 -0500

    se ha ingresado algo en html

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 console.log('hola mundo');
 /* git init inicializa tu proyecti 
 
+git status verifica que archivos se estan guardando
+git add agrega los archivos a git para que se guarden
+git commit crea un punto de control
+git diff index.html ve las diferencias que se agrego
+git checkout -- regresa los cambios
 
 */
